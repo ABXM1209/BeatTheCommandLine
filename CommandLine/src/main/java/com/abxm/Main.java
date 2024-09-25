3b8122bd3553f6f8917788663ef68b26168ec3cf
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2024-09-25 11:04:30 +0200

    Hey man how is it going?

--- CommandLine/src/main/java/com/abxm/Main.java
+++ CommandLine/src/main/java/com/abxm/Main.java
@@ -3,5 +3,6 @@ package com.abxm;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("How is it going?!");
     }
 }
